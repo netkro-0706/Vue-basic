e1fd0760133ca3338137b489f322ace9e7b8d98b
netkro <kroging@gmail.com>
2025-09-30 21:11:06 +0900

    ogernaizing folder

---
+++ Vue/Vue3-beginner/5.Single-file-component/vue3/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <AppHeader v-bind:appTitle="message" v-on:change="changeMessage" />
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue"
+
+export default {
+  components: {
+    AppHeader,
+  },
+  data() {
+    return {
+      message: "앱 헤더 컴포넌트",
+    }
+  },
+  methods: {
+    changeMessage() {
+      this.message = "변경됨"
+    },
+  },
+}
+</script>
+
+<style scoped></style>
