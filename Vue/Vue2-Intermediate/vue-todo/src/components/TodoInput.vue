1355b0d2e043f48fc305056c1bd1817a70a218a0
netkro <kroging@gmail.com>
2025-10-01 14:23:41 +0900

    todoinput

--- Vue/Vue2-Intermediate/vue-todo/src/components/TodoInput.vue
+++ Vue/Vue2-Intermediate/vue-todo/src/components/TodoInput.vue
@@ -1,9 +1,61 @@
 <template>
-  <div>Input</div>
+  <div class="inputBox shadow">
+    <input
+      type="text"
+      ref="inputTodo"
+      @keydown.enter="addTodo"
+      v-model="newTodoItem"
+    />
+    <!-- <button class="addBtn" v-on:click="addTodo">add</button> -->
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="fa-solid fa-plus"></i>
+    </span>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput() {
+      this.newTodoItem = "";
+      this.$refs.inputTodo.focus();
+    },
+  },
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>
