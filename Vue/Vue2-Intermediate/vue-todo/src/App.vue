e1674fd306c995270e8f24febbf9159cc668a8dd
netkro <kroging@gmail.com>
2025-10-06 23:30:27 +0900

    change to helper in components

--- Vue/Vue2-Intermediate/vue-todo/src/App.vue
+++ Vue/Vue2-Intermediate/vue-todo/src/App.vue
@@ -15,9 +15,7 @@ import TodoList from "./components/TodoList.vue";
 
 export default {
   data() {
-    return {
-      // todoItems: [],
-    };
+    return {};
   },
   methods: {},
   components: {
