2a454ee9c6c18a50f7493a125b53edff7fda6a81
netkro <kroging@gmail.com>
2025-10-08 18:37:56 +0900

    add todoInput

--- Vue3-composition/2.vue3-todo/src/App.vue
+++ Vue3-composition/2.vue3-todo/src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <TodoHeader />
+  <TodoInput />
+  <TodoList />
 </template>
 
 <script>
 import TodoHeader from "./components/TodoHeader.vue"
+import TodoInput from "./components/TodoInput.vue"
+import TodoList from "./components/TodoList.vue"
 
 export default {
   components: {
     TodoHeader,
+    TodoInput,
+    TodoList,
   },
 }
 </script>
---
+++ Vue3-composition/2.vue3-todo/src/components/TodoInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="text" ref="inputRef" v-model="todoInput" />
+    <button @click="addTodo">추가</button>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue"
+
+export default {
+  setup() {
+    const todoInput = ref("")
+    const inputRef = ref(null)
+
+    const addTodo = async () => {
+      const todo = todoInput.value
+      localStorage.setItem(todo, todo)
+      todoInput.value = ""
+      inputRef.value.focus()
+    }
+
+    return { todoInput, addTodo, inputRef }
+  },
+}
+</script>
+
+<style scoped></style>
