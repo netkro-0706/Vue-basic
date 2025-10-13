132f2e38004a69c3066f8c3c9212654ef971ddb4
netkro <kroging@gmail.com>
2025-10-13 22:38:56 +0900

    setting

--- Vue2-end/vue-til/.eslintrc.js
+++ Vue2-end/vue-til/.eslintrc.js
@@ -12,8 +12,22 @@ module.exports = {
     parser: "@babel/eslint-parser",
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-console": "off",
+    // "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: false,
+        semi: true,
+        // useTabs: true,
+        // tabWidth: 2,
+        // trailingComma: "all",
+        // bracketSpacing: false,
+        // arrowParens: "avoid",
+        printWidth: 80,
+      },
+    ],
   },
   overrides: [
     {
