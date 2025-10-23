d71ce417be374c1ce93ec71199e18a6db0790a60
netkro <kroging@gmail.com>
2025-10-23 18:49:27 +0900

    add create post page

--- Vue2-end/vue-til/src/views/MainPage.vue
+++ Vue2-end/vue-til/src/views/MainPage.vue
@@ -11,12 +11,15 @@
         />
       </ul>
     </div>
+    <router-link to="/add" class="create-button">
+      <i class="ion-md-add"></i>
+    </router-link>
   </div>
 </template>
 
 <script>
 import { fetchPosts } from "@/api/index"
-import LoadingSpinner from "@/components/posts/LoadingSpinner.vue";
+import LoadingSpinner from "@/components/common/LoadingSpinner.vue";
 import PostListItem from "@/components/posts/PostListItem.vue"
 
 export default {
