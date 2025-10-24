d527ae7ef6fe0ef10e1cb24c98e463c0f4dd5854
netkro <kroging@gmail.com>
2025-10-24 11:14:57 +0900

    api의 모듈화

---
+++ Vue2-end/vue-til/src/api/auth.js
@@ -0,0 +1,15 @@
+// 로그인, 회원가입
+import {instance} from "./index"
+
+// 회원가입
+function registerUser(userData){
+  return instance.post("signup", userData)
+}
+
+// 로그인
+function loginUser(userData){
+  return instance.post("login", userData);
+}
+
+
+export {registerUser, loginUser}
--- Vue2-end/vue-til/src/api/index.js
+++ Vue2-end/vue-til/src/api/index.js
@@ -1,35 +1,19 @@
 import axios from "axios"
 import { setInterceptors } from "./common/interceptors"
 
+function createInstance(){
+  return axios.create({
+    baseURL: process.env.VUE_APP_API_URL,
+  })
+}
 
 // axios초기화
-function createInstance(){
+function createInstanceWithAuth(url){
   const instance = axios.create({
-    baseURL: process.env.VUE_APP_API_URL,
+    baseURL: `${process.env.VUE_APP_API_URL}${url}`,
   });
   return setInterceptors(instance);
 }
 
-const instance = createInstance();
-
-// 회원가입
-function registerUser(userData){
-  return instance.post("signup", userData)
-}
-
-// 로그인
-function loginUser(userData){
-  return instance.post("login", userData);
-}
-
-// 학습 노트 데이터를 조회하는 API
-function fetchPosts(){
-  return instance.get("posts");
-}
-
-//학습노트 데이터를 생성하는 API
-function createPost(postData){
-  return instance.post("posts", postData);
-}
-
-export { registerUser, loginUser, fetchPosts, createPost }
+export const instance = createInstance();
+export const posts = createInstanceWithAuth("posts");
